90dbea9fc2f38727fd1e719abae7be67416ce1af
Lunarixus <Nathan@lunarixus.dev>
2023-10-18 09:33:38 +0530

    vince: Remove some more packages
    - Also switch to platform certificate
    
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,8 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Drive ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
